0c0fa5bf1dd1273d98bb8b358750bf92e6834019
dougcwest <93220603+dougcwest@users.noreply.github.com>
2022-10-27 19:47:30 -0500

    feat: Token search form

--- components/DescriptionSections/TokenForms.styled.tsx
+++ components/DescriptionSections/TokenForms.styled.tsx
@@ -1,9 +1,6 @@
 import styled from 'styled-components';
 import { Main } from 'styles/Library/Button.styled';
-import {
-  SectionTitleContainer,
-  Title as DescTitle,
-} from './Description.styled';
+import { SectionTitleContainer, Title as DescTitle } from './Description.styled';
 
 export const Container = styled.div`
   display: flex;
@@ -40,3 +37,24 @@ export const Input = styled.input`
 `;
 
 export const Button = styled(Main)``;
+
+export const IdFormLabel = styled.h4`
+  color: ${(props) => props.theme.colors.textMain};
+`;
+
+export const Asterisk = styled.p`
+  color: ${(props) => props.theme.colors.textOffset};
+  font-size: 13px;
+  margin-top: 4px;
+  font-style: italic;
+`;
+
+export const SmallButton = styled(Button)`
+  min-height: 30px;
+  min-width: 30px;
+  height: 37px;
+  width: 37px;
+  font-size: 25px;
+  padding: 0;
+  margin-left: -10px;
+`;
