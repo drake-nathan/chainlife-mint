6e7f153797c474d314e09c34944bbdb8a799b712
Nathan Drake <nathandrakedrums@gmail.com>
2022-11-21 15:54:48 -0600

    fix: Inability to go to Zen section

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -37,74 +37,72 @@ const Home: NextPage = () => {
         <meta name="description" content="Chainlife." />
       </Head>
 
-      <>
-        <NavBar />
-        <St.BodyContainer>
-          <St.SliderAndIframeContainer>
-            {windowWidth > 1000 ? (
-              <Slider>
-                {sliderTokens.map((token) => (
-                  <div
-                    key={token.id}
-                    className="tool"
-                    data-tip={`Chainlife # ${token.id}`}
-                    onClick={() => setActiveToken({ genUrl: token.genUrl, id: token.id })}
-                  >
-                    <img src={token.thumbUrl} alt="nft" />
-                  </div>
-                ))}
-              </Slider>
-            ) : null}
-            <St.LeftSection>
-              <St.TitleAnCryptoContainer>
-                <St.TitleContainer>
-                  <St.Title>
-                    {currentSupply
-                      ? `${currentSupply} of ${maxSupply} minted`
-                      : `${maxSupply} max supply.`}
-                  </St.Title>
-                  {activeToken && (
-                    <St.SubTitle>Showing Chainlife # {activeToken.id}</St.SubTitle>
-                  )}
-                </St.TitleContainer>
-                {windowWidth > 750 ? <Web3Buttons /> : null}
-              </St.TitleAnCryptoContainer>
-              {isTxPending ? (
-                <LoadingVideo />
-              ) : (
-                activeToken && (
-                  <iframe
-                    height={
-                      windowWidth > 750
-                        ? '650'
-                        : windowWidth >= 412
-                        ? '412'
-                        : windowWidth >= 390
-                        ? '390'
-                        : '360'
-                    }
-                    width={windowWidth > 750 ? '650' : '390'}
-                    src={activeToken.genUrl}
-                    title="generator"
-                    frameBorder="0"
-                  ></iframe>
-                )
-              )}
+      <NavBar />
+      <St.BodyContainer>
+        <St.SliderAndIframeContainer>
+          {windowWidth > 1000 ? (
+            <Slider>
+              {sliderTokens.map((token) => (
+                <div
+                  key={token.id}
+                  className="tool"
+                  data-tip={`Chainlife # ${token.id}`}
+                  onClick={() => setActiveToken({ genUrl: token.genUrl, id: token.id })}
+                >
+                  <img src={token.thumbUrl} alt="nft" />
+                </div>
+              ))}
+            </Slider>
+          ) : null}
+          <St.LeftSection>
+            <St.TitleAnCryptoContainer>
+              <St.TitleContainer>
+                <St.Title>
+                  {currentSupply
+                    ? `${currentSupply} of ${maxSupply} minted`
+                    : `${maxSupply} max supply.`}
+                </St.Title>
+                {activeToken && (
+                  <St.SubTitle>Showing Chainlife # {activeToken.id}</St.SubTitle>
+                )}
+              </St.TitleContainer>
+              {windowWidth > 750 ? <Web3Buttons /> : null}
+            </St.TitleAnCryptoContainer>
+            {isTxPending ? (
+              <LoadingVideo />
+            ) : (
+              activeToken && (
+                <iframe
+                  height={
+                    windowWidth > 750
+                      ? '650'
+                      : windowWidth >= 412
+                      ? '412'
+                      : windowWidth >= 390
+                      ? '390'
+                      : '360'
+                  }
+                  width={windowWidth > 750 ? '650' : '390'}
+                  src={activeToken.genUrl}
+                  title="generator"
+                  frameBorder="0"
+                ></iframe>
+              )
+            )}
 
-              <St.InfoContainer>
-                {windowWidth < 750 ? <Web3Buttons /> : null}
-                <St.InfoText>
-                  Chainlife NFTs are on-chain, generative-art, collectible and evolving
-                  games, that together create an everchanging, 3D world.
-                </St.InfoText>
-              </St.InfoContainer>
-            </St.LeftSection>
-          </St.SliderAndIframeContainer>
-          <St.DescriptionsContainer>
-            <DescriptionSections />
-          </St.DescriptionsContainer>
-        </St.BodyContainer>
-      </>
+            <St.InfoContainer>
+              {windowWidth < 750 ? <Web3Buttons /> : null}
+              <St.InfoText>
+                Chainlife NFTs are on-chain, generative-art, collectible and evolving
+                games, that together create an everchanging, 3D world.
+              </St.InfoText>
+            </St.InfoContainer>
+          </St.LeftSection>
+        </St.SliderAndIframeContainer>
+        <St.DescriptionsContainer>
+          <DescriptionSections />
+        </St.DescriptionsContainer>
+      </St.BodyContainer>
     </St.AppContainer>
   );
 };
