40f7eeea3c02baca09049a18014a9fe1be33d781
Matto <monkmatto@protonmail.com>
2024-10-19 16:47:14 -0400

    added click to change notes

--- components/Instructions/steps.ts
+++ components/Instructions/steps.ts
@@ -1,12 +1,12 @@
 export const instructions = [
   [
-    "To explore specific levels and tokens, click anywhere on the viewer so it's active, then press ' \\ ' to open the input field.",
+    "To explore specific levels and tokens, click anywhere on the viewer so it's active, then press ' \\ ' to open the input field. (click to continue)",
   ],
   [
     "In the input field, enter 'L' and a number (eg. L0) to view a single level of the world. The input field will disappear, and the following hotkeys become active:",
     "Press 'E' to export that level's pattern.",
     "Press 'P' to play that level's pattern.",
-    "Pressing any other key will return to the world view.",
+    "Pressing any other key will return to the world view. (click to continue)",
   ],
   [
     "In the input field, enter 'T' and the token-ID (eg. T1234) to highlight an individual token. The input field will disappear, and the following hotkeys become active:",
@@ -15,6 +15,6 @@ export const instructions = [
     "Press 'V' to view that token's 'Esoterra'.",
     "Press 'E' to export that token's level's pattern.",
     "Press 'P' to play that token's level's pattern.",
-    "Pressing any other key will return to the world view.",
+    "Pressing any other key will return to the world view. (click to exit)",
   ],
 ];
